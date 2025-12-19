30f487c98fa6284e93e48121ce8fa62c3310d8e6
Akash Iyer <akashiyer2022@gmail.com>
2025-12-19 15:43:14 -0500

    Change base path in Vite config to root

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: set this to your repo name
-  // example: https://akashiyer2022.github.io/akashiyer2022-netizen/
-  base: "/akashiyer2022-netizen/",
+  base: "/",
 });
